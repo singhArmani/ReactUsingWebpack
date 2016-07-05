b85d7479e07af372cb57d0f349e3696084250328
Aman Singh <singh_aman@hotmail.com.au>
2016-07-06 09:13:18 +1000

    install jsx-loader added into the loaders array in webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -5,16 +5,19 @@ var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
 var APP_DIR = path.resolve(__dirname, 'src/client/app');
 
 var config = {
-    entry: APP_DIR + '/index.jsx',
+
+    entry: ['webpack-dev-server/client?http://0.0.0.0:3000', // WebpackDevServer host and port
+            'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
+            APP_DIR + '/index.jsx'], //app's entry point
     output: {
         path: BUILD_DIR,
         filename: 'bundle.js'
     },
     module: {
         loaders:[{
-            test: /\.jsx?/,
+            test: /\.jsx?$/,
             include: APP_DIR,
-            loader: 'babel'
+            loader: ['babel','react-hot', 'jsx?harmony']
         }]
     }
 
