8f0919bd8732fe9629b565a8df9c73348308a6a3
Aman Singh <singh_aman@hotmail.com.au>
2016-07-06 08:49:06 +1000

    Created a component using ES6 classes and then updating the bundle.js by running ./node_modules/.bin/webpack -d command

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class App extends React.Component {
+    render(){
+        return <p>Hello React!</p>;
+    }
+}
+
+ReactDom.render(<App />, document.getElementById('app'))
