7b395e365fe27d5ffbe9bbb1dbbfdf4cf0ce3554
Aman Singh <singh_aman@hotmail.com.au>
2016-07-13 03:16:15 +1000

    Created Shipping Component and added event handlers and proper states.

--- components/ShippingDetails.js
+++ components/ShippingDetails.js
@@ -7,12 +7,47 @@ export default class ShippingDetails extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {Address:'-'};
+        this.state = {firstName:'',contactNumber:'', shippingAddress:''};
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleSubmit(event){
+        event.preventDefault();
+        console.log('form has been submitted');
+    }
+    
+    handleChange(event,attribute){
+        var newState = this.state; //grabbing the intial value of state here
+        newState[attribute] = event.target.value; //putting the new changed value into respective attribute
+        this.setState(newState); //setting this as new state
     }
     
     render(){
         return (
-            <h1>Please Enter your Shipping Details</h1>
+            <div>
+                <h3>Please Enter your Shipping Details</h3>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <label>FirstName: </label>
+                            <input type="text"  className="form-control" value={this.state.firstName} onChange={(event) => this.handleChange(event,'firstName')}/>
+                        </div>
+
+                        <div className="form-group">
+                            <label>ContactNumber: </label>
+                            <input type="text" className="form-control" value={this.state.contactNumber} onChange={(event) => this.handleChange(event,'contactNumber')}/>
+                        </div>
+
+                        <div className="form-group">
+                            <label> Shipping Address: </label>
+                            <input type="text" className="form-control" value={this.state.shippingAddress} onChange={(event) => this.handleChange(event,'shippingAddress')}/>
+                        </div>
+
+                            <button className="btn btn-success" type="submit">Submit</button>
+                    </form>
+
+            </div>
         );
     }
 }
