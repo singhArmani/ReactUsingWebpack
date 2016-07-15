39acc37be00a0f11c67389f179f4a0677781d3c0
Aman Singh <singh_aman@hotmail.com.au>
2016-07-16 06:07:59 +1000

    confirmation page created and diplay all user selected data

---
+++ components/Confirmation.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export default class Confirmation extends React.Component {
+    constructor(props){
+        super(props);
+
+    }
+    handleSubmit(event){
+        event.preventDefault();
+        this.props.updateFormData(this.props.data);
+    }
+
+    render(){
+        return (
+            <div>
+                <h1>Are you sure you want to submit the data? </h1>
+                    <form onSubmit ={this.handleSubmit}>
+                        <div>
+                            <strong>Full Name</strong>:{this.props.data.fullName}
+                        </div><br/>
+
+                        <div>
+                            <strong>Contact Number</strong>:{this.props.data.contactNumber}
+                        </div><br/>
+
+                        <div>
+                            <strong>ShippingAddress</strong>:{this.props.data.shippingAddress}
+                        </div><br/>
+
+                        <div>
+                            <strong>Selected Books</strong>:{this.props.data.selectedBooks.join(",")}
+                        </div><br/>
+
+                        <button className="btn btn-success">Place Order</button>
+                    </form>
+            </div>
+        );
+    }
+}
