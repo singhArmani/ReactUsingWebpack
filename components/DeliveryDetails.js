fe51ad3727f4030cf16852be8a40b1a56c3a41fc
Aman Singh <singh_aman@hotmail.com.au>
2016-07-16 05:48:10 +1000

    DeliveryDetails Component created and implemented successfully.

--- components/DeliveryDetails.js
+++ components/DeliveryDetails.js
@@ -7,12 +7,59 @@ export default class ShippingDetails extends React.Component {
 
     constructor(props){
         super(props);
-        this.state = {Address:'-'};
+        this.state = {delivaryOption:'Primary'};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+
+    }
+
+    handleSubmit(){
+        event.preventDefault();
+        console.log("form submitted!");
+        this.props.updateFormData(this.state); 
+
+    }
+
+    handleChange(event){
+
+        this.setState({delivaryOption:event.target.value}); //setting a new state
+
+
     }
 
     render(){
         return (
-            <h1>Choose your Delivery Options Here.</h1>
+            <div>
+                <h1>Choose your Delivery Options Here.</h1>
+                <div style={{width:200}}>
+                    <form action="" onSubmit={this.handleSubmit}>
+                        <div className="radio">
+                            <label>
+                                <input type="radio"
+                                       value="Primary"
+                                       onChange={this.handleChange}
+                                        checked={this.state.delivaryOption === 'Primary'}
+                                />
+                                Pirmary--Next Day Delivery
+                            </label>
+                        </div>
+
+                        <div className="radio">
+                            <label>
+                                <input type="radio"
+                                       value="Normal"
+                                       onChange ={this.handleChange}
+                                       checked={this.state.delivaryOption === 'Normal'}
+                                />
+                                Normal -- 3-4 Days
+                            </label>
+                        </div>
+                        <button className="btn btn-success">
+                            Submit
+                        </button>
+                    </form>
+                </div>
+            </div>
         );
     }
 }
